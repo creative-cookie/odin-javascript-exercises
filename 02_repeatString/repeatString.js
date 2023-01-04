7987b9ccb918db8f4c93e3a0ad2409cc42f3e7f7
creative-cookie <38289561+creative-cookie@users.noreply.github.com>
2023-01-04 18:54:55 -0500

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = '';
+    if(num < 0){
+        newString = 'ERROR';
+        return newString;
+    }
 
+    while(num){
+        newString += string;
+        num--;
+    }
+    return newString;
 };
 
 // Do not edit below this line
