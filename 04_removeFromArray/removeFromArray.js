40f11975e284fc911cd567c00787e70c366f272c
creative-cookie <38289561+creative-cookie@users.noreply.github.com>
2023-01-09 20:46:59 -0500

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(nums, ...args) {
+    return nums.filter((num) =>{
+        for(const arg of args){
+            if(arg === num){
+                return false;
+            } 
+        }
+        return true;
+    });
 };
 
+//console.log(removeFromArray([1, 2, 3, 4], 3, 2))
 // Do not edit below this line
 module.exports = removeFromArray;
