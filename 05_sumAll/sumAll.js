e7841760568270ef5d95606bd40075582c645d0d
creative-cookie <38289561+creative-cookie@users.noreply.github.com>
2023-01-11 19:47:49 -0500

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let low;
+    let high;
 
+    if(Number.isInteger(num1) && Number.isInteger(num2)){
+        if(Math.sign(num1) === 1 && Math.sign(num2) === 1){
+            if(num1 !== num2){
+                if(num1 < num2){
+                    low = num1;
+                    high = num2;
+                }else{
+                    low = num2;
+                    high = num1;
+                }
+            }
+        }else{
+            return 'ERROR';
+        }
+    }else{
+        return 'ERROR';
+    }
+
+    for(; low <= high; low++){
+        sum += low;
+    }
+    return sum;
 };
 
 // Do not edit below this line
