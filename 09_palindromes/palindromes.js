2ec61497462bdad5df368e49a4050ea4f877eae4
creative-cookie <38289561+creative-cookie@users.noreply.github.com>
2023-07-15 19:25:31 -0400

    complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let forward = [...str].map(letter => letter.toLowerCase()).filter(letter => letter.match(/[a-z]/));
+    let backward = [...forward].reverse();
 
+    return (forward.toString() === backward.toString()) ? true : false;
 };
 
 // Do not edit below this line
