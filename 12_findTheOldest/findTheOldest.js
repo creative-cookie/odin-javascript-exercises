5552ab488e2f1b62e43e51d8c9f7669808d00569
creative-cookie <38289561+creative-cookie@users.noreply.github.com>
2023-07-16 16:46:34 -0400

    complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((acc, person, index) =>{
+        let currentPersonAge;
+        let prevPersonAge;
 
+        if(index === 0){
+            return acc = {...person};
+        }
+
+        if("yearOfDeath" in person){
+            currentPersonAge = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            currentPersonAge = (new Date().getFullYear()) - person.yearOfBirth;
+        }
+
+        if("yearOfDeath" in acc){
+            prevPersonAge = acc.yearOfDeath - acc.yearOfBirth;
+        } else {
+            prevPersonAge = (new Date().getFullYear()) - acc.yearOfBirth;
+        }
+        
+        if(currentPersonAge > prevPersonAge){
+            return acc = {...person};
+        }else {
+            return acc;
+        }
+    }, {})
 };
 
 // Do not edit below this line
