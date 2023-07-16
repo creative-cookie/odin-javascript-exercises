a5b4d58c66ab0c1bec61b1cabb223406b839b17e
creative-cookie <38289561+creative-cookie@users.noreply.github.com>
2023-07-16 10:28:54 -0400

    complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) { return 'OOPS'};
+    if(num === 0) { return 0};
 
+    let arr = [];
+    for(let i = 0; i <= num; i++){
+        (i === 0 || i === 1) ? arr.push(1) : arr.push(arr[i-1] + arr[i-2])
+    }
+
+    return arr[num-1];
 };
 
 // Do not edit below this line
